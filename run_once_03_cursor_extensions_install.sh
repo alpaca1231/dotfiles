5ee924c0828d2fd8a11a18ef7e775691f080d3cd
alpaca1231 <kazuki.amatatsu.fd@gmail.com>
2025-01-29 21:53:55 +0900

    fix: run_once_03_cursor_extensions_install.sh

--- run_once_03_cursor_extensions_install.sh
+++ run_once_03_cursor_extensions_install.sh
@@ -2,7 +2,7 @@
 
 if command -v cursor &> /dev/null; then
     echo "Installing Cursor extensions"
-    xargs -L 1 cursor --install-extension < ~/.config/chezmoi/.cursor/extensions.txt
+    xargs -L 1 cursor --install-extension < ~/.cursor/extensions.txt
 else
   echo "Cursorコマンドがインストールされていません"
 fi
