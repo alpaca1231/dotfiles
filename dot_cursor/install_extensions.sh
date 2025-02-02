bf3cdbdbfe3696a5831e57a83e1b5f317d0b2023
alpaca1231 <kazuki.amatatsu.fd@gmail.com>
2025-02-02 10:58:57 +0900

    fix: remove cursor extension and add profile

--- dot_cursor/install_extensions.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env zsh
-
-if command -v cursor &> /dev/null; then
-    echo "Installing Cursor extensions"
-    xargs -L 1 cursor --install-extension < ~/.cursor/extensions.txt
-else
-  echo "Cursorコマンドがインストールされていません"
-fi
